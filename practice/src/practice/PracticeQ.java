51906aab4a360b8768620a41c2fa8bc18ac5db51
0ee90-coder <dantallian0419@gmail.com>
2026-07-16 00:23:38 +0900

    과제 진행

---
+++ practice/src/practice/PracticeQ.java
@@ -0,0 +1,65 @@
+package practice;
+
+public class PracticeQ {
+
+	public static void main(String[] args) {
+		int money = 1_000_000;
+		int father = 40;
+		int mother = 36;
+		int daughter = 11;
+		int adultOneWayFlightFare = 300_000;
+		int kidOneWayFlightFare = 120_000;
+		
+		int adultcount = 0;
+		int kidcount = 0;
+		
+		if (father >= 19) {
+			adultcount = adultcount +1;
+		}
+		else {
+			kidcount = kidcount +1;
+		}
+		
+		if (mother >= 19) {
+			adultcount = adultcount +1;
+		}
+		else {
+			kidcount = kidcount +1;
+		}
+		
+		if (daughter >= 19) {
+			adultcount = adultcount +1;
+		}
+		else {
+			kidcount = kidcount +1;
+		}
+		System.out.println(adultcount +"명");
+		System.out.println(kidcount +"명");
+		
+		int adultFare = adultcount * adultOneWayFlightFare;
+		int kidFare = kidcount * kidOneWayFlightFare;
+		int totalFare = adultFare + kidFare;
+		
+		System.out.println(totalFare +"원");
+		
+		if (totalFare <= money) {
+			System.out.println("여행가자!");
+		}
+		else {
+			System.out.println("다음에가자!");
+		}
+		
+		// 3인 가족이 100만원으로 비행기를 타고 편도 여행을 가려합니다.
+		// 부모님의 나이는 각각 40, 36세입니다.
+		// 딸의 나이는 11세입니다.
+		// 성인의 비행요금은 30만원
+		// 아동의 비행요금은 12만원입니다. 성인 판단 기준은 19세 이상입니다.
+		// 3인 가족은 여행을 떠날 수 있을까요?
+		// 여행을 떠날 수 있다면 "여행가자!"
+		// 여행을 떠날 수 없다면 "다음에가자"
+		// 를 출력해보세요.
+		}
+
+	
+	
+}
--- practice_0715/src/practice_0715/HomeWork0716.java
+++ practice_0715/src/practice_0715/HomeWork0716.java
@@ -13,8 +13,39 @@ public class HomeWork0716 {
 		System.out.println(minusResult);
 		System.out.println(multiResult);
 		System.out.println(devideResult);
+	}
+	//2. 정수 한 개를 받아서 짝수라면 true를, 홀수라면 false를 반환하는 메소드를 만들고 호출해보세요.
+	public static boolean isEven(int num) {
+		return num % 2 == 0;
 		
 	}
+	
+	//3. 정수 세 개를 받아서 가장 큰 수를 출력하는 메소드를 만들고 호출해보세요.
+	public static int isMax(int num1 , int num2 , int num3) {
+		
+		int max = num1;
+		if (num2 > max) {
+			max = num2;
+		}
+		if (num3 > max) {
+			max = num3;
+		}
+		return max;
+	}	
+	
+	//4. 정수 한 개를 받아서 소수(prime number)라면 true를 아니라면 false를 반환하는 메소드를 만들고 호출해 보세요.
+	public static boolean isPrimeNumber(int num4) {
+		
+		if(num4 % 2 == 0) {
+			
+		}
+		
+		if(num4 % 3 == 0) {
+			
+		}
+		
+	}
+		
 	public static void main(String[] args) {
 		
 		Numbers(2,5);
@@ -22,16 +53,9 @@ public class HomeWork0716 {
 		Numbers(2,5);
 		Numbers(2,5);
 		
-	//2. 정수 한 개를 받아서 짝수라면 true를, 홀수라면 false를 반환하는 메소드를 만들고 호출해보세요.
-//	public static boolean Number(int number) {
-//		if(number % 2 == 0) {
-//			
-//		}
-//	}	
-	
-	//3. 정수 세 개를 받아서 가장 큰 수를 출력하는 메소드를 만들고 호출해보세요.
-
-	//4. 정수 한 개를 받아서 소수(prime number)라면 true를 아니라면 false를 반환하는 메소드를 만들고 호출해 보세요.
-	
+		System.out.println(isEven(4));
+			
+		System.out.println(isMax (5,9,7));
+			
 	}
 }
